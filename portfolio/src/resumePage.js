28e9d0ca25053a5fa92a3a14aa415d6c6f576b4a
mishell778 <mishell.r.123@gmail.com>
2026-02-15 23:10:09 -0500

    update resume

--- portfolio/src/resumePage.js
+++ portfolio/src/resumePage.js
@@ -1,7 +1,7 @@
 import MovingGradient from "./components/MovingGradience";
 
 const ResumePage = () => {
-  const pdfPath = `${process.env.PUBLIC_URL}/cardenas_espinosa_resume6.pdf`;
+  const pdfPath = `${process.env.PUBLIC_URL}/cardenas_espinosa_resume7.pdf`;
 
   return (
     <>
